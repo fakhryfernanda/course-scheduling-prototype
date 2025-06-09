35c1e9b22fbf5cad125e5ff5a0d00b1f4cc7291e
Fakhry Fernanda <mff023@gmail.com>
2025-06-09 17:48:17 +0700

    Setup parameter for NSGA

--- globals.py
+++ globals.py
@@ -1,8 +1,22 @@
+import random
 from typing import Final
+from itertools import product
 from enums.evaluation_method import EvaluationMethod
 from dataframes.curriculum import Curriculum
 from dataframes.subject import Subject
 from dataframes.room import Room
+
+# Define ranges
+crossover_rates = [round(0.6 + 0.03 * i, 2) for i in range(11)]
+mutation_rates = [round(0.01 + 0.03 * i, 2) for i in range(9)]
+mutation_points = list(range(1, 6))
+
+# Generate all combinations
+all_combinations = list(product(crossover_rates, mutation_rates, mutation_points))
+
+# Pick one random index
+idx = random.randint(0, len(all_combinations) - 1)
+
 
 subjects = Subject("csv/subjects.csv")
 curriculum = Curriculum("csv/curriculum.csv", subjects.df)
@@ -17,9 +31,11 @@ SLOTS_PER_DAY: Final[int] = 5
 
 POPULATION_SIZE: Final[int] = 100
 MAX_GENERATION: Final[int] = 100
-CROSSOVER_RATE: Final[float] = 0.7
-MUTATION_RATE: Final[float] = 0.2
-MUTATION_POINTS: Final[int] = 5
+
+# CROSSOVER_RATE: Final[float] = 0.7
+# MUTATION_RATE: Final[float] = 0.2
+# MUTATION_POINTS: Final[int] = 5
+CROSSOVER_RATE, MUTATION_RATE, MUTATION_POINTS = all_combinations[idx]
 
 IS_MULTI_OBJECTIVE: Final[bool] = True
 EVALUATION_METHOD = EvaluationMethod.AVERAGE_SIZE
